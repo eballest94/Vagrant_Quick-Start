9ee882d40f14562ffc96646dc6fd9002413f6bf2
Edson Ballestas E <eballest@cisco.com>
2020-07-14 18:00:15 -0500

    adding tomcat script support

--- provision-tomcat.sh
+++ provision-tomcat.sh
@@ -4,3 +4,13 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y nano git openjdk-7-jdk openjdk-7-jre-headless
 apt-get install -y apache2
+
+cd /usr/local
+wget https://downloads.apache.org/tomcat/tomcat-7/v7.0.105/bin/apache-tomcat-7.0.105.tar.gz
+tar -xzvf apache-tomcat-7.0.105.tar.gz
+ln -s apache-tomcat-7.0.105 tomcat
+
+cp /vagrant/tomcat8 /home/vagrant/tomcat8
+chmod 755 /home/vagrant/tomcat8
+update-rc.d tomcat8 defaults
+service tomcat8 start
